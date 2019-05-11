7804b0bae8727362498a82dd69295e149cda1d0b
Renato <Renato>
2019-05-11 15:13:18 +0100

    tic tac toe

--- index.js
+++ index.js
@@ -2,35 +2,53 @@ main = {};
 
 main.value = '';
 
-main.start = () => {
-    document.getElementById('start').addEventListener('click', () => {
-
-    })
-
-}
+let gameNumber = new Array();
 
 main.valuesSelected = [];
 main.clickX = () => {
     document.getElementById('x').addEventListener('click', ($event) => {
+        event.preventDefault();
+        const msgWin = document.getElementById('msg-error').innerText;
         document.getElementById('msg-error').innerText = '';
-        main.value = ($event.target.id);
+        if (msgWin != undefined) {
+            main.value = ($event.target.id);
+        } else {
+            document.getElementById('msg-error').innerText = 'Game Over';
+        }
     });
 }
 main.click0 = () => {
     document.getElementById('o').addEventListener('click', ($event) => {
+        event.preventDefault();
+        const msgWin = document.getElementById('msg-error').innerText;
         document.getElementById('msg-error').innerText = '';
-        main.value = $event.target.id;
+        if (msgWin != 'Game Over') {
+            main.value = ($event.target.id);
+        }
+    });
+}
+
+main.reset = () => {
+    document.getElementById('reset').addEventListener('click', ($event) => {
+        event.preventDefault();
+        let msgWin = document.getElementById('msg-error').textContent;
+        if (msgWin != undefined) {
+            for (let i = 1; i < 9; i++) {
+                document.getElementById(`rect${i}`).innerText = '';
+            }
+            window.location.reload(true)
+        }
     });
 }
 
 main.groupClick = () => {
     const elem = document.getElementsByClassName('rect');
+    event.preventDefault();
     for (let i = 0; i < elem.length; i++) {
         elem[i].addEventListener('click', ($event) => {
             main.startAlert($event);
             main.rectDom($event);
             main.win();
-            2
         });
     }
 }
@@ -44,73 +62,79 @@ main.startAlert = ($event) => {
 
 }
 
-main.putValue = ($event) => {
+main.putValue = ($event) => { 
     const idClicked = $event.target.id;
-    if (!$event.target.innerText && main.valuesSelected.length > 0 && main.valuesSelected.pop() == main.value) {
+    if (!$event.target.innerText && main.valuesSelected.length > 0 &&  main.valuesSelected.pop() == main.value) {
         document.getElementById('msg-error').innerText = 'Ups! now is another player';
         document.getElementById(idClicked).innerHTML = '';
-        main.valuesSelected.push(main.value);
 
     } else {
         main.valuesSelected.push(main.value);
+        if(main.value == 'x') {
+            document.getElementById(idClicked).style.color = 'red';
+        }
         document.getElementById(idClicked).innerHTML = `<p>${main.value}</p>`;
-    }
+    } 
 }
 
 main.win = () => {
     const mainObject = main.objectPlayer;
     const arrayOptions = ['x', 'o'];
     for (let i = 0; i < arrayOptions.length; i++) {
-        if (mainObject.rect1 == arrayOptions[i] && mainObject.rect4 == arrayOptions[i],
+        if (mainObject.rect1 == arrayOptions[i] && mainObject.rect4 == arrayOptions[i] &&
             mainObject.rect7 == arrayOptions[i]) {
-            document.getElementById('msg-error').innerText = 'Game Over';
+            document.getElementById('msg-error').innerText = `'Game Over the ${ arrayOptions[i]} has won!`;
 
         }
 
     }
     for (let i = 0; i < arrayOptions.length; i++) {
-        if (mainObject.rect2 == arrayOptions[i] && mainObject.rect5 == arrayOptions[i],
+        if (mainObject.rect2 == arrayOptions[i] && mainObject.rect5 == arrayOptions[i] &&
             mainObject.rect8 == arrayOptions[i]) {
-            document.getElementById('msg-error').innerText = 'Game Over';
-        }
+                document.getElementById('msg-error').innerText = `'Game Over the ${ arrayOptions[i]}`;
+            }
 
     }
     for (let i = 0; i < arrayOptions.length; i++) {
-        if (mainObject.rect3 == arrayOptions[i] && mainObject.rect6 == arrayOptions[i],
+        if (mainObject.rect3 == arrayOptions[i] && mainObject.rect6 == arrayOptions[i] &&
             mainObject.rect9 == arrayOptions[i]) {
-            document.getElementById('msg-error').innerText = 'Game Over';
-        }
+                document.getElementById('msg-error').innerText = `'Game Over the ${ arrayOptions[i]}`;
+            }
     }
     for (let i = 0; i < arrayOptions.length; i++) {
-        if (mainObject.rect1 == arrayOptions[i] && mainObject.rect2 == arrayOptions[i],
+        if (mainObject.rect1 == arrayOptions[i] && mainObject.rect2 == arrayOptions[i] &&
             mainObject.rect3 == arrayOptions[i]) {
-            document.getElementById('msg-error').innerText = 'Game Over';
-        }
+                document.getElementById('msg-error').innerText = `'Game Over the ${ arrayOptions[i]}`;
+            }
     }
     for (let i = 0; i < arrayOptions.length; i++) {
-        if (mainObject.rect4 == arrayOptions[i] && mainObject.rect5 == arrayOptions[i],
+        if (mainObject.rect4 == arrayOptions[i] && mainObject.rect5 == arrayOptions[i] &&
             mainObject.rect6 == arrayOptions[i]) {
-            document.getElementById('msg-error').innerText = 'Game Over';
-        }
+                document.getElementById('msg-error').innerText = `'Game Over the ${ arrayOptions[i]}`;
+            }
     }
     for (let i = 0; i < arrayOptions.length; i++) {
-        if (mainObject.rect7 == arrayOptions[i] && mainObject.rect8 == arrayOptions[i],
+        if (mainObject.rect7 == arrayOptions[i] && mainObject.rect8 == arrayOptions[i] &&
             mainObject.rect9 == arrayOptions[i]) {
-            document.getElementById('msg-error').innerText = 'Game Over';
-        }
+                document.getElementById('msg-error').innerText = `'Game Over the ${ arrayOptions[i]}`;
+            }
     }
     for (let i = 0; i < arrayOptions.length; i++) {
-        if (mainObject.rect1 == arrayOptions[i] && mainObject.rect5 == arrayOptions[i],
+        if (mainObject.rect1 == arrayOptions[i] && mainObject.rect5 == arrayOptions[i] &&
             mainObject.rect9 == arrayOptions[i]) {
-            document.getElementById('msg-error').innerText = 'Game Over';
-        }
+                document.getElementById('msg-error').innerText = `'Game Over the ${ arrayOptions[i]}`;
+            }
     }
 
     for (let i = 0; i < arrayOptions.length; i++) {
-        if (mainObject.rect3 == arrayOptions[i] && mainObject.rect5 == arrayOptions[i],
+        if (mainObject.rect3 == arrayOptions[i] && mainObject.rect5 == arrayOptions[i] &&
             mainObject.rect7 == arrayOptions[i]) {
-            document.getElementById('msg-error').innerText = 'Game Over';
-        }
+                document.getElementById('msg-error').innerText = `'Game Over the ${ arrayOptions[i]}`;
+            }
+    }
+    gameNumber.push(main.value);
+    if (gameNumber.length >= 10) {
+        document.getElementById('msg-error').innerText = "Cats game!";
     }
 }
 main.rectDom = (event) => {
@@ -128,10 +152,13 @@ main.clickGameArea = () => {
 }
 
 main.init = () => {
+   document.getElementById('container').style.backgroundImage = "url('tee.jpeg')";
+   document.getElementById('container').style.opacity = 0.1;
     main.clickX();
     main.click0();
     main.groupClick();
     main.win();
+    main.reset();
 }
 
 window.onload = () => {
